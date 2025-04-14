8c160c74383bcd1cd2698f60823f1cb0c3daec5c
tomharding-dev <dev-tomh@rding.uk>
2025-04-14 16:58:02 +0100

    add asteroid splitting, completed base game

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,7 @@
+import random
 from circleshape import *
+from constants import *
+
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +12,20 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+        
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        angle = random.uniform(20, 50)
+        
+        velocity1 = (self.position.rotate(angle))
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        velocity2 = (self.position.rotate(-angle))
+        
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = velocity1 * 0.5
+        
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2.velocity = velocity2 * 0.5
